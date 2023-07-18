b45a6ea1f0c2b02d4f894ba0a28e59dff0860986
Jimesosa59 <139372440+Jimesosa59@users.noreply.github.com>
2023-07-18 18:05:56 -0400

    juego wordle listo

--- script.js
+++ script.js
@@ -1,38 +1,67 @@
-let palabra = "ARBOL"
+//let lista = ["ARBOL","ANGEL","MARCO", "CARRO", "BARRO"]
+// let palabra = "ARBOL"
+
+let palabra = "" //lista[Math.floor(Math.random()*lista.length)];
+
 let intentos = 6;
+
+const API = "https://random-word-api.herokuapp.com/word?length=5&lang=es"
+fetch(API)
+.then(response => response.json())
+.then(response => {
+    console.log(response)
+    palabra= response[0] .toUpperCase()
+    console.log(palabra) 
+})
+.catch(err => palabra = lista[Math.floor(Math.random()*lista.length)])
 
 const BOTON = document.getElementById("guess-button");
 BOTON.addEventListener("click", intentar)
 
-function intentar(){
+function intentar(){                      
     const INTENTO = leerIntento();
-    if (INTENTO === palabra) {
-        terminar ("<h1>ganaste:)</h1>")
-        return
+    if (INTENTO == palabra){
+        terminar("<h1>ganaste :) </h1>")
+        //return
     }
+    const GRID = document.getElementById("grid");
+    const ROW = document.createElement('div');
+    ROW.className = 'row';
+    
     for (let i in palabra){
-        if (palabra[i]==INTENTO[i]){
-            alert("verde")
+        const SPAN = document.createElement('span');
+        SPAN.className = 'letter';
+        if ( palabra[i]=== INTENTO[i]){ 
+            SPAN.innerHTML = INTENTO[i];
+            SPAN.style.backgroundColor = 'green';
+        } else if( palabra.includes(INTENTO[i]) ) { 
+            SPAN.innerHTML = INTENTO[i];
+            SPAN.style.backgroundColor = 'yellow';
+        } else { 
+            SPAN.innerHTML = INTENTO[i];
+            SPAN.style.backgroundColor = 'grey';
         }
-       else if (palabra.includes(INTENTO[i]) ){
-        alert(INTENTO[i]+" amarillo")
-       }
-       else  {
-        alert(INTENTO[i]+" gris")
-       }
-     }
+        ROW.appendChild(SPAN)
+    }
+    GRID.appendChild(ROW) 
      intentos--
      if (intentos==0){
         terminar("<h1>perdiste:(</h1>")
      }
     }  
-function leerIntento(){
+
+    function leerIntento(){
     let intento = document.getElementById("guess-input").value;
-   intento = intento.toUpperCase()
+    intento = intento.toUpperCase()
     return intento
     }
     function terminar(mensaje){
+        BOTON.disabled = true;
         let contenedor = document.getElementById("guesses");
         contenedor.innerHTML= mensaje;
     
     }
+
+    
+
+   
